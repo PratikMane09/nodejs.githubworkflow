55b8af948e0619d14c4141133845cb44a81e1cf4
pratikdossiefoyer <pratik.mane@dossiefoyer.in>
2025-05-19 16:18:02 +0530

    adding a workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
+export default [
+  // Base JavaScript rules
+  js.configs.recommended,
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+  // Add browser globals
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  // React rules
+  {
+    files: ["**/*.{jsx,js}"],
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+    },
+  },
+];
